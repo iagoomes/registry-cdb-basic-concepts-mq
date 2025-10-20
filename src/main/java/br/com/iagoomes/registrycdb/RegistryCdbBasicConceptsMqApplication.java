e0ecc2c8ae6b99d30ef7bff589592ae02d249a90
Iago Gomes <iagoomes@outlook.com>
2025-10-20 08:28:29 -0300

    chore: initial commit

---
+++ src/main/java/br/com/iagoomes/registrycdb/RegistryCdbBasicConceptsMqApplication.java
@@ -0,0 +1,13 @@
+package br.com.iagoomes.registrycdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryCdbBasicConceptsMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryCdbBasicConceptsMqApplication.class, args);
+	}
+
+}
